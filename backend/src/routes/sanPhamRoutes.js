df7eb3bf3c2e8ca3026e9e8b8bb5bbef22a0910c
Tevel <thuathan64@gmail.com>
2026-08-12 20:27:58 +0700

    しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん しかのこのこのここしたんたん

--- backend/src/routes/sanPhamRoutes.js
+++ backend/src/routes/sanPhamRoutes.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const sanPhamController = require('../controllers/sanPhamController');
 
-// Route lấy danh sách sản phẩm
-router.get('/', sanPhamController.getSanPhamAll);
 
-// Route lấy chi tiết sản phẩm theo ID
+// Tìm kiếm và lọc sản phẩm (Phải đặt trước /:id)
+router.get('/search', sanPhamController.searchAndFilterSanPham);
+
+// Lấy chi tiết sản phẩm theo ID
 router.get('/:id', sanPhamController.getSanPhamById);
+// Lấy danh sách tất cả sản phẩm
+
+router.get('/', sanPhamController.getSanPhamAll);
 
 module.exports = router;
