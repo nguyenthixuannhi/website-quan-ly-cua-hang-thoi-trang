9f741ad49c6d72ad85b4ea4955c4c2cb835971da
Tevel <thuathan64@gmail.com>
2026-08-19 15:48:13 +0700

    FEAT: Add getRelatedProducts function and update routes and tests

--- backend/src/routes/sanPhamRoutes.js
+++ backend/src/routes/sanPhamRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const sanPhamController = require('../controllers/sanPhamController');
 
 router.get('/search', sanPhamController.searchAndFilterSanPham);
+router.get('/related/:id', sanPhamController.getRelatedProducts);
 router.get('/', sanPhamController.getAll);
 router.get('/:id', sanPhamController.getById);
 router.post('/', sanPhamController.create);
