99c7536e23d69ef424087894fa16bf8b5e1c546f
Tevel <thuathan64@gmail.com>
2026-08-23 21:07:29 +0700

    i forgot i already did thhatt....

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -82,28 +82,11 @@ async function me(req, res) {
   return res.status(200).json({ user: req.user });
 }
 
-// Get /api/auth/verify-token
-function verifyToken(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Không tìm thấy token' });
-  }
-
-  const token = authHeader.split(' ')[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'YOUR_SECRET_KEY');
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: 'Token không hợp lệ hoặc đã hết hạn' });
-  }
-}
 
 
 module.exports = {
   register,
   login,
   me,
-  verifyToken,
 };
 
